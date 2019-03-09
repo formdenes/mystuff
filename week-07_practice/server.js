d50b2a2877f77073979a4f9b6f7e1911c4f6c237
formdenes <formdenes@gmial.com>
2019-03-09 16:02:34 +0100

    Setted up view engine

--- week-07_practice/server.js
+++ week-07_practice/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//Requiring npm modules
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -7,6 +8,14 @@ const mysql = require('mysql');
 require('dotenv').config();
 app.use(express.json());
 
+//Connecting to local mysql database
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
-})
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+
+//Use ejs as template view engine, from view folder
+app.set('view engine', 'ejs');
